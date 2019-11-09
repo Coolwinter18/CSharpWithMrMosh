ff12c71d0fdc12351c952948b1b3608400b7a128
Carlos Eduardo Ferreyra <48495214+Coolwinter18@users.noreply.github.com>
2019-11-09 16:09:16 -0300

    Primer Commit desde VS

---
+++ Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * __Convension de Nombres__
+             * Camel Case: firstName
+             * Pascal Case: FirstName
+             * Hungarian Notation: StrFirstName
+            */
+
+
+        }
+    }
+}
