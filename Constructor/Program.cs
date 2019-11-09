01f979ae34f32fa9e7cde1808e424efbda756771
Carlos Eduardo Ferreyra <48495214+Coolwinter18@users.noreply.github.com>
2019-11-09 18:50:34 -0300

    Constructor 1

---
+++ Constructor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Customer
+    {
+        //atributos
+        public int Id;
+        public string Name;
+        //constructores
+        public Customer() //constructor
+        {
+
+        }
+        public Customer(string name) //otro constructor
+        {
+            this.Name = name;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer("John");
+
+        }
+    }
+}
